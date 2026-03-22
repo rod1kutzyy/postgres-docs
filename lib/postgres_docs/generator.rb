b44dc029b830145421ad78a1714d6199140939f5
rehy <ysazeka@gmail.com>
2026-03-22 17:47:42 +0300

    Add logic of building diagram(scheme)

--- lib/postgres_docs/generator.rb
+++ lib/postgres_docs/generator.rb
@@ -10,7 +10,7 @@ module PostgresDocs
     def generate
       document_block = [
         build_header,
-        build_mm_diagram,
+        build_mermaid_diagram,
         build_toc,
         build_table_datails
       ]
@@ -27,6 +27,28 @@ module PostgresDocs
         > Generated automaticly by gem `postgres_docs`.
         > Count of tables: **#{@schema.keys.size} **
       MARKDOWN
+    end
+
+    def build_mermaid_diagram # rubocop:disable Metrics/AbcSize
+      lines = ["Scheme of database (ER-diagramm)", "", "```mermaid", "erDiagram"]
+      @schema.each do |table_name, data|
+        data[:foreign_keys].each do |fk|
+          lines << "    #{fk[:foreign_table]} ||--o{ #{table_name} : \"#{fk[:column]}\""
+        end
+
+        lines << "    #{table_name} {"
+        data[:columns].each do |col|
+          key_mark = col[:is_pk] ? " PK" : ""
+          key_mark = " FK" if data[:foreign_keys].any? { |fk| fk[:column] == col[:name] }
+
+          safe_type = col[:type].gsub(/\s+/, "_")
+          lines << "        #{safe_type} #{col[:name]}#{key_mark}"
+        end
+        lines << "    }"
+      end
+
+      lines << "```"
+      lines.join("\n")
     end
   end
 end
