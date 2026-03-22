2768ff73c490d677ce5812554db8b9b323e6de8c
rehy <ysazeka@gmail.com>
2026-03-22 17:01:58 +0300

    Refactoring. Add fetch_db, generate_data, write_to_file

--- lib/postgres_docs/cli.rb
+++ lib/postgres_docs/cli.rb
@@ -10,21 +10,45 @@ module PostgresDocs
     end
     
     desc "generate DSN", "Generate Markdown-documintation for PostgreSQL data-base"
-    method_optin :output,
+    method_option :output,
                   type: :string,
                   aliases: "-o",
-                  default: "database_docs.md"
+                  default: "database_docs.md",
                   desc: "Path for output file (example: docs/db.md)"
     
     
     def generate(dsn)
+      # All blocks of generating
+    rescue StandardError => e
+        say "Failed: #{e.message}", :red
+        exit 1   
+    end
+
+    def fetch_db(dsn)
       say "Connecting to database...", :cyan
-      begin
-        #logic of getting info from db
-        say "Scheme was successfully extracted. Tables founded: #{}", :green
-      rescue StandartError => e
-        say "Error while working with database. More info: #{}", :red
-        exit 1
-    end            
+      #logic of getting info from db
+      say "Scheme was successfully extracted. Tables founded: #{}", :green
+    rescue StandardError => e
+      say "Error while working with database. More info: #{e.message}", :red
+      exit 1
+    end
+      
+    def generate_data(scheme)
+      say "Generating MD & Mermaid-scheme...", :cyan
+      #logic of generation
+    rescue => e
+      say "Generation error: #{e.message}", :red
+      exit 1
+    end
+
+     
+    def write_to_file(path,content) 
+      File.write(options[:output], md_content)
+      say "Documentation successfully saved into: #{options[:output]}", :green
+    rescue => e
+      say "Writing to file error: #{e.message}", :red
+      exit 1
+    end
+
   end
 end
