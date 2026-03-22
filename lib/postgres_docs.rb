c470d3a8306e76cddd78bd2a4c721900e51a49dc
Rodion Agutin <rodionagutin@gmail.com>
2026-03-22 18:04:09 +0300

    Add imports in postgres_docs.rb

--- lib/postgres_docs.rb
+++ lib/postgres_docs.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "postgres_docs/version"
+require_relative "postgres_docs/database"
+require_relative "postgres_docs/generator"
+require_relative "postgres_docs/cli"
 
 module PostgresDocs
   class Error < StandardError; end
-  # Your code goes here...
 end
